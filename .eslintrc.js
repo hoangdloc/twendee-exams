fa59255acc53f19c909d2c02c54abfaefd4be2b1
Hoang Loc <hoanggdloc@gmail.com>
2023-03-23 18:48:19 +0700

    Setup style guide, eslint, tailwind, redux

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'standard-with-typescript'
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json']
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+    indent: ['error', 2, { SwitchCase: 1 }],
+    semi: ['error', 'always'],
+    '@typescript-eslint/semi': 'off'
+  }
+};
--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -1,3 +1,7 @@
+/* eslint-disable @typescript-eslint/strict-boolean-expressions */
+/* eslint-disable @typescript-eslint/no-floating-promises */
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+/* eslint-disable @typescript-eslint/consistent-type-imports */
 import { ReportHandler } from 'web-vitals';
 
 const reportWebVitals = (onPerfEntry?: ReportHandler) => {
